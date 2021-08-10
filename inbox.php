fda41327f83e4d6b589e5a3b4b12479e2f27834f
versha-G <76777874+versha-G@users.noreply.github.com>
2021-08-10 08:17:51 +0530

    Add files via upload

---
+++ inbox.php
@@ -0,0 +1,41 @@
+<?php include_once "header.php"; ?>
+  <body>
+    <div class="wrapper">
+      <section class="chat-area">
+        <header>
+          <a href="#" class="back-icon"><i class="fas fa-arrow-left"></i></a>
+          <div class="details">
+            <span> Versha </span>
+            <p>Active Now</p>
+          </div>
+        </header>
+        <div class="chat-box">
+          <div class="chat outgoing">
+            <div class="details">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis autem fugit ab? Similique qui doloribus vel impedit
+                voluptatum sapiente porro nihil pariatur omnis eos, atque
+                aperiam. Repellendus eius ex consectetur.
+              </p>
+            </div>
+          </div>
+          <div class="chat incoming">
+            <div class="details">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Blanditiis autem fugit ab? Similique qui doloribus vel impedit
+                voluptatum sapiente porro nihil pariatur omnis eos, atque
+                aperiam. Repellendus eius ex consectetur.
+              </p>
+            </div>
+          </div>
+        </div>
+        <form action="#" class="typing-area">
+          <input type="text" placeholder="Type a message here..." />
+          <button><i class="fab fa-telegram-plane"></i></button>
+        </form>
+      </section>
+    </div>
+  </body>
+</html>
